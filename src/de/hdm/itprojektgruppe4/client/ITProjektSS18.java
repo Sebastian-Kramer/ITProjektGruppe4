056bc6ec0601e041f06287e52fea4dc4281e2e23
Georg <georg.erich.1993@gmail.com>
2018-04-30 14:52:32 +0200

    Test

--- src/de/hdm/itprojektgruppe4/client/ITProjektSS18.java
+++ src/de/hdm/itprojektgruppe4/client/ITProjektSS18.java
@@ -29,12 +29,12 @@ public class ITProjektSS18 implements EntryPoint {
 			+ "attempting to contact the server. Please check your network " + "connection and try again.";
 
 	/**
-	 * Create a  remote service proxy to talk to the server-side Greeting service.
+	 * Create aa  remote service proxy to talk to the server-side Greeting service.
 	 */
 	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 
 	/**
-	 * This is the entry point method.
+	 * This is the entry point method. 
 	 */
 	public void onModuleLoad() {
 		final Button sendButton = new Button("Send");
