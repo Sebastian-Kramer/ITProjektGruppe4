f525abf6fde4d8c9494b5d5ed9cc626108d68a4e
Georg <georg.erich.1993@gmail.com>
2018-05-06 23:39:56 +0200

    BO Teilhaberschaft
    Attribute angelegt
    Teilhaberschaft Getter & Setter angelegt
    
    - TeilhaberschaftMapper 
    
    Klasse bearbeitet
    findByID angelegt

--- src/de/hdm/itprojektgruppe4/server/db/TeilhaberschaftMapper.java
+++ src/de/hdm/itprojektgruppe4/server/db/TeilhaberschaftMapper.java
@@ -1,5 +1,63 @@
 package de.hdm.itprojektgruppe4.server.db;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import de.hdm.itprojektgruppe4.shared.bo.Teilhaberschaft;
+
 public class TeilhaberschaftMapper {
+	
+	private static TeilhaberschaftMapper teilhaberschaftMapper = null;
+	
+	protected TeilhaberschaftMapper(){
+		
+	};
+	
+	public static TeilhaberschaftMapper teilhaberschaftMapper(){
+		
+		if(teilhaberschaftMapper == null){
+			
+			teilhaberschaftMapper = new TeilhaberschaftMapper();
+		}
+		
+		return teilhaberschaftMapper;
+	}
+	
+	public Teilhaberschaft findByID(int id) {
+		
+		Connection con = DBConnection.connection();
+
+		try {
+			
+			Statement stmt = con.createStatement();
+
+			ResultSet rs = stmt.executeQuery(
+					
+					"SELECT `ID`, `Kontaktliste_ID`, `Kontakt_ID`, `Eigenschaft_ID`,"
+					+ " `Eigenschaftsauspraegung_ID`, `Teilhaber_ID` FROM `teilhaberschaft` WHERE ID=" + id);
+
+			if (rs.next()) {
+				
+				Teilhaberschaft t = new Teilhaberschaft();
+				
+				t.setID(rs.getInt("id"));
+				t.setKontaktListeID(rs.getInt("id"));
+				t.setKontaktID(rs.getInt("id"));
+				t.setEigenschaftsID(rs.getInt("id"));
+				t.setEigenschaftsAuspraegungID(rs.getInt("id"));
+				t.setTeilhaberID(rs.getInt("id"));
+
+				return t;
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+			
+			return null;
+		}
+		
+		return null;
+	}
 
 }
--- src/de/hdm/itprojektgruppe4/shared/bo/Teilhaberschaft.java
+++ src/de/hdm/itprojektgruppe4/shared/bo/Teilhaberschaft.java
@@ -2,4 +2,68 @@ package de.hdm.itprojektgruppe4.shared.bo;
 
 public class Teilhaberschaft extends BusinessObject{
 
+	private static final long serialVersionUID = 1L;
+
+	
+	int NutzerID; 
+	
+	int KontaktID;
+	
+	int KontaktListeID;
+	
+	int EigenschaftsID;
+	
+	int TeilhaberID;
+	
+	public int getTeilhaberID() {
+		return TeilhaberID;
+	}
+
+	public void setTeilhaberID(int teilhaberID) {
+		TeilhaberID = teilhaberID;
+	}
+
+	public int getEigenschaftsID() {
+		return EigenschaftsID;
+	}
+
+	public void setEigenschaftsID(int eigenschaftsID) {
+		EigenschaftsID = eigenschaftsID;
+	}
+
+	int EigenschaftsAuspraegungID;
+	
+
+	public int getNutzerID() {
+		return NutzerID;
+	}
+
+	public void setNutzerID(int nutzerID) {
+		NutzerID = nutzerID;
+	}
+
+	public int getKontaktID() {
+		return KontaktID;
+	}
+
+	public void setKontaktID(int kontaktID) {
+		KontaktID = kontaktID;
+	}
+
+	public int getKontaktListeID() {
+		return KontaktListeID;
+	}
+
+	public void setKontaktListeID(int kontaktListeID) {
+		KontaktListeID = kontaktListeID;
+	}
+
+	public int getEigenschaftsAuspraegungID() {
+		return EigenschaftsAuspraegungID;
+	}
+
+	public void setEigenschaftsAuspraegungID(int eigenschaftsAuspraegungID) {
+		EigenschaftsAuspraegungID = eigenschaftsAuspraegungID;
+	}
+	
 }
