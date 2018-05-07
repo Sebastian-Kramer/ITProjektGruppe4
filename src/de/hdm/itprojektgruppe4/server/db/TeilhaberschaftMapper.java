e1d0bd3f1eca9a6bef648bd9bce1a21bc9659934
Georg <georg.erich.1993@gmail.com>
2018-05-07 23:19:45 +0200

    TeihaberschaftMapper
    Insert Teilhaberschaft erstellt
    Delete Teilhaberschaft erstellt
    DeleteKontaktFromTeilhaberschaft erstellt
    DeleteKontaktlisteFromTeilhaberschaft erstellt
    DeleteEigenschaftFromTeilhaberschaft erstellt
    DeleteEigenschaftsAuspraegung erstellt

--- src/de/hdm/itprojektgruppe4/server/db/TeilhaberschaftMapper.java
+++ src/de/hdm/itprojektgruppe4/server/db/TeilhaberschaftMapper.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import de.hdm.itprojektgruppe4.shared.bo.Teilhaberschaft;
 
+
 public class TeilhaberschaftMapper {
 	
 	private static TeilhaberschaftMapper teilhaberschaftMapper = null;
@@ -59,5 +60,137 @@ public class TeilhaberschaftMapper {
 		
 		return null;
 	}
+	
+	
+	  public Teilhaberschaft insert(Teilhaberschaft t) {
+		    Connection con = DBConnection.connection();
+
+		    try {
+		      Statement stmt = con.createStatement();
+
+		      ResultSet rs = stmt.executeQuery("SELECT MAX(id) AS maxid "
+		          + "FROM teilhaberschaft ");
+
+		      if (rs.next()) {
+		
+		        t.setID(rs.getInt("maxid") + 1);
+
+		        stmt = con.createStatement();
+
+		        stmt.executeUpdate("INSERT INTO teilhaberschaft (`ID`, `Kontaktliste_ID`, `Kontakt_ID`, "
+		        		+ "`Eigenschaft_ID`, `Eigenschaftsauspraegung_ID`, `Teilhaber_ID`) " + 
+		        		"VALUES ("+ t.getID() + "," + t.getKontaktListeID() + 
+		        		"," + t.getKontaktID() + "," + t.getEigenschaftsID() + 
+		        		"," + t.getEigenschaftsAuspraegungID() + "," + t.getTeilhaberID() +")");
+		      }
+		    }
+		    catch (SQLException e2) {
+		      e2.printStackTrace();
+		    }
+
+		    return t;
+		  }
+	  
+	
+	  
+	  public void delete(Teilhaberschaft t) {
+		  
+		    Connection con = DBConnection.connection();
+
+		    try {
+		    	
+		      Statement stmt = con.createStatement();
+
+		      stmt.executeUpdate("DELETE FROM teilhaberschaft " + "WHERE id=" + t.getID());
+
+		    }
+		    catch (SQLException e2) {
+		    	
+		      e2.printStackTrace();
+		      
+		    }
+		    
+		  }
+	  
+	  public void deleteKontaktFromTeilhaberschaft(Teilhaberschaft t) {
+		  
+		    Connection con = DBConnection.connection();
+
+		    try {
+		    	
+		      Statement stmt = con.createStatement();
+
+		      stmt.executeUpdate("DELETE FROM teilhaberschaft " + 
+		      "WHERE Kontakt_ID=" + t.getKontaktID());
+
+		    }
+		    catch (SQLException e2) {
+		    	
+		      e2.printStackTrace();
+		      
+		    }
+		    
+		  }
+	  
+	  public void deleteKontaktkisteFromTeilhaberschaft(Teilhaberschaft t) {
+		  
+		    Connection con = DBConnection.connection();
+
+		    try {
+		    	
+		      Statement stmt = con.createStatement();
+
+		      stmt.executeUpdate("DELETE FROM teilhaberschaft " + 
+		      "WHERE Kontaktliste_ID=" + t.getKontaktListeID());
+
+		    }
+		    catch (SQLException e2) {
+		    	
+		      e2.printStackTrace();
+		      
+		    }
+		    
+		  }
+	  
+	  public void deleteEigenschaftFromTeilhaberschaft(Teilhaberschaft t) {
+		  
+		    Connection con = DBConnection.connection();
+
+		    try {
+		    	
+		      Statement stmt = con.createStatement();
+
+		      stmt.executeUpdate("DELETE FROM teilhaberschaft " + 
+		      "WHERE Eigenschaft_ID=" + t.getEigenschaftsID());
+
+		    }
+		    catch (SQLException e2) {
+		    	
+		      e2.printStackTrace();
+		      
+		    }
+		    
+		  }
+	  
+	  public void deleteEigenschaftsAuspraegungFromTeilhaberschaft(Teilhaberschaft t) {
+		  
+		    Connection con = DBConnection.connection();
+
+		    try {
+		    	
+		      Statement stmt = con.createStatement();
+
+		      stmt.executeUpdate("DELETE FROM teilhaberschaft " + 
+		      "WHERE Eigenschaftsauspraegung_ID=" + t.getEigenschaftsAuspraegungID());
+
+		    }
+		    catch (SQLException e2) {
+		    	
+		      e2.printStackTrace();
+		      
+		    }
+		    
+		  }
 
 }
+
