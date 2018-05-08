b4c639ed59db68dab16459098ea4f84e27cd0cb4
MarcusReiss <mr154@hdm-stuttgart.de>
2018-05-08 13:33:26 +0200

    KontaktlisteMapper Methoden hinzugefuegt und getestet

--- src/de/hdm/itprojektgruppe4/server/db/KontaktlisteMapper.java
+++ src/de/hdm/itprojektgruppe4/server/db/KontaktlisteMapper.java
@@ -1,8 +1,9 @@
-package de.hdm.itprojektgruppe4.server.db;
+	package de.hdm.itprojektgruppe4.server.db;
 
 import java.sql.*;
 import java.util.Vector;
 
+import de.hdm.itprojektgruppe4.shared.bo.Kontakt;
 import de.hdm.itprojektgruppe4.shared.bo.Kontaktliste;
 
 public class KontaktlisteMapper {
@@ -54,5 +55,94 @@ public class KontaktlisteMapper {
 			return null;
 			
 		}
+	public Kontaktliste findbyBezeichnung(String bezeichnung){
+		Connection con = DBConnection.connection();
+		
+		try{
+			Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT ID, Bez, Eigentuemer_ID FROM Kontaktliste " + "WHERE Bez="
+			+ bezeichnung);
+		
+		if(rs.next()){
+			Kontaktliste k = new Kontaktliste();
+			k.setID(rs.getInt("ID"));
+			k.setBezeichnung(rs.getString("Bez"));
+			k.setNutzerID(rs.getInt("Eigentuemer_ID"));
+			return k ;
+		
+		
+	
+				}
+			}
+		catch(SQLException e2){
+			e2.printStackTrace();
+			return null;
+		}
+		return null;
+	}
+	
+	
+		public Kontaktliste createKontaktliste(Kontaktliste k) {
+		
+		Connection con = DBConnection.connection(); 
+		
+		try{
+			
+			Statement stmt = con.createStatement();
+			
+			ResultSet rs = stmt.executeQuery("SELECT MAX(ID) AS maxID " + " FROM kontaktliste ");
+			
+			if (rs.next()) {
+				
+				k.setID(rs.getInt("maxID") +1);
+				
+				stmt = con.createStatement();
+				
+				stmt.executeUpdate(
+						"INSERT INTO Kontaktliste (ID, Bez, Eigentuemer_ID)" + " VALUES (" + k.getID() + "x ,'" + k.getBezeichnung() 
+						+ "' ,'" + k.getNutzerID() + "')");
+					
+						
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
 
+		}	
+		
+		return k;
+}
+		public Kontaktliste updateKontaktliste(Kontaktliste k) {
+			Connection con = DBConnection.connection();
+			
+			try{
+				
+				Statement stmt = con.createStatement();
+				
+				stmt.executeUpdate("UPDATE Kontaktliste " + "SET Bez=\"" 
+				+ k.getBezeichnung() +"\", " 
+				+ "Eigentuemer_ID=\"" 
+				+ k.getNutzerID() + "\" " 
+				+ "WHERE ID=" + k.getID());
+				
+				
+				
+			}catch (SQLException e){
+				e.printStackTrace();
+			}
+			return k;
+		}
+		
+		
+		public void deleteKontaktliste(Kontaktliste k){
+			Connection con = DBConnection.connection();
+			
+			try {
+				
+				Statement stmt = con.createStatement();
+				
+				stmt.executeUpdate("DELETE FROM Kontaktliste " + "WHERE ID= " + k.getID());
+			}catch (SQLException e){
+				e.printStackTrace();
+			}
+		}
 }
