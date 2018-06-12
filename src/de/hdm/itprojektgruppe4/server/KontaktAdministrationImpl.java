53582ae6887ac6f27a1aeb20ad3a72057df1ef9d
NinoDoering <nd040@hdm-stuttgart.de>
2018-06-12 19:02:14 +0200

    BugFix für das Bearbeiten der Ausprägungen eines neuen Kontaktes.

--- src/de/hdm/itprojektgruppe4/server/KontaktAdministrationImpl.java
+++ src/de/hdm/itprojektgruppe4/server/KontaktAdministrationImpl.java
@@ -1411,12 +1411,14 @@ public class KontaktAdministrationImpl extends RemoteServiceServlet
 			newVec.add(kontaktliste);
 			
 			
-			if (kontaktliste.getBez().equals("Meine Kontakte")) {
+			if (kontaktliste.getBez().equals("Meine Kontakte") || kontaktliste.getBez().equals("Meine geteilten Kontakte")) {
 				
 				newVec.remove(kontaktliste);
 			
 			}
 		}
+		
+		
 			
 		alleListen = newVec;
 		
@@ -1425,7 +1427,26 @@ public class KontaktAdministrationImpl extends RemoteServiceServlet
 		
 	}
 
-
+//	public KontaktKontaktliste removeExistingKontakt(int kontaktID, int kontaktlisteID, int nutzerID) throws IllegalArgumentException {
+//		
+//		Vector<Kontaktliste> alleListen = findKontaktlisteByNutzerID(nutzerID);
+//		
+//		Vector<KontaktKontaktliste> alleKonKonlistbyKontakt = kontaktKontaktlisteMapper.findKontaktKontaktlisteByKontaktID(kontaktID);
+//		
+//		Vector<Kontaktliste> newAlleListen = new Vector<Kontaktliste>();
+//		
+//		Vector<KontaktKontaktliste> newalleKonKonlist = new Vector<KontaktKontaktliste>();
+//		
+//		for (Kontaktliste kontaktliste : alleListen) {
+//			newAlleListen.add(kontaktliste);
+//		}
+//		
+//		return null;
+//		
+//		
+//	}
+	
+	
 
 	@Override
 	public Vector<Kontakt> findKontaktByNameAndNutzerID(Kontakt kontakt) throws IllegalArgumentException {
