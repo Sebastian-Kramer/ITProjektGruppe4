addefa12479d18184cbc1e60fa3595e342a5ebc6
MarcusReiss <mr154@hdm-stuttgart.de>
2018-06-14 20:30:10 +0200

    shared.Report Klassen aufgeräumt/Kommentiert

--- src/de/hdm/itprojektgruppe4/shared/report/ReportWriter.java
+++ src/de/hdm/itprojektgruppe4/shared/report/ReportWriter.java
@@ -1,10 +1,41 @@
 package de.hdm.itprojektgruppe4.shared.report;
 
+/**
+ *  <p>
+ * Diese Klasse wird benötigt, um auf dem Client die ihm vom Server zur
+ * Verfügung gestellten <code>Report</code>-Objekte in ein menschenlesbares
+ * Format zu überführen.
+ * </p>
+ * <p>
+ * Das Zielformat kann prinzipiell beliebig sein. Methoden zum Auslesen der in
+ * das Zielformat überführten Information wird den Subklassen überlassen. In
+ * dieser Klasse werden die Signaturen der Methoden deklariert, die für die
+ * Prozessierung der Quellinformation zuständig sind.
+ * </p>
+ * Code in Anlehnung an Bankprojekt von Prof.Thies
+ *
+ */
+
 public abstract class ReportWriter {
+	
+	/**
+	 * Übersetzt einen <code>KontakteMitBestimmtenEIgenschaftsAuspraegungenReport</code> in das Zielformat.
+	 * @param c
+	 */
 	
 	public abstract void process(KontakteMitBestimmtenEigenschaftsAuspraegungenReport c);
 	
+	/**
+	 * Übersetzt einen <code>AllEigeneKontakteReport</code> in das Zielformat
+	 * @param c
+	 */
+	
 	public abstract void process(AllEigeneKontakteReport c); 
+	
+	/**
+	 * Übersetzt einen <code>KontaktMitBestimmterTeilhaberschaftReport</code> in das Zielformat
+	 * @param c
+	 */
 	
 	public abstract void process(KontakteMitBestimmterTeilhaberschaftReport c);
 	
