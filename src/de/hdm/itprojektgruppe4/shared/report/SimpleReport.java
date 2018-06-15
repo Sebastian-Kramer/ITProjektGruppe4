039833818f58c0b1955636c59c2a8cbd0fd96700
MarcusReiss <mr154@hdm-stuttgart.de>
2018-06-15 18:15:39 +0200

    Klasse SimpleReport kommentiert

--- src/de/hdm/itprojektgruppe4/shared/report/SimpleReport.java
+++ src/de/hdm/itprojektgruppe4/shared/report/SimpleReport.java
@@ -13,17 +13,33 @@ import java.util.Vector;
 	
 		private static final long serialVersionUID = 1L ;
 		
+		/**
+		 *  die Tabelle wird zeilenhweise in diesem Vektor abgelegt
+		 */
 		private Vector<Row> table = new Vector<Row>();
+		
+		/**
+		 * fügt eine Zeile hinzu
+		 * @param r die hinzuzufügende Zeile
+		 */
 		
 		  public void addRow(Row r) {
 			    this.table.addElement(r);
 			  }
 		  
+		  /**
+		   * entfernt eine Zeile
+		   * @param r  die zu entfernende Zeile
+		   */
 		  
 		  public void removeRow(Row r) {
 			    this.table.removeElement(r);
 			  }
 		  
+		 /**
+		  *  Auslesen der Positionsdatenaus der Tabelle
+		  * @return Tabelle der Positionsdaten
+		  */
 		  public Vector<Row> getRows() {
 			    return this.table;
 		  	}
