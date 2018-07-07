d9f570ca913941ea1f71e780bd3dba8df50c91ef
Sebastian-Kramer <sk265@hdm-stuttgart.de>
2018-07-07 17:57:30 +0200

    1) Anpassung

--- src/de/hdm/itprojektgruppe4/server/db/PersonMapper.java
+++ src/de/hdm/itprojektgruppe4/server/db/PersonMapper.java
@@ -5,129 +5,124 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import de.hdm.itprojektgruppe4.shared.bo.Kontakt;
 import de.hdm.itprojektgruppe4.shared.bo.Person;
 
 /**
- * Mapper Klassen, die <code>Person</code>-Objekte auf einer relationalen Datenbank abbildet.
+ * Mapper Klassen, die <code>Person</code>-Objekte auf einer relationalen
+ * Datenbank abbildet.
  *
  */
 
 public class PersonMapper {
 
 	/**
-     * Der Mapper ist ein Singelton.
-     * Variable ist "static" und speichert die einzige Instanz dieser Klasse.
-     */
-	
-	private static PersonMapper personMapper = null;
-	/**
-	 * geschützter Konstruktor, der verhindert, dass Klasse mit "new" instanziert wird.
+	 * Der Mapper ist ein Singelton. Variable ist "static" und speichert die
+	 * einzige Instanz dieser Klasse.
 	 */
-	
+
+	private static PersonMapper personMapper = null;
+
+	/**
+	 * geschützter Konstruktor, der verhindert, dass Klasse mit "new"
+	 * instanziert wird.
+	 */
+
 	protected PersonMapper() {
 	};
-	
-	
+
 	public static PersonMapper personMapper() {
 		if (personMapper == null) {
 			personMapper = new PersonMapper();
 		}
 		return personMapper;
-	}	
-	
+	}
+
 	/**
 	 * Suchen einer Person mit vorgegebener ID.
-	 * @param id der Person
+	 * 
+	 * @param id
+	 *            der Person
 	 * @return die gesuchte Person
-	 */		
-	
-public Person findPersonByID(int id){
-		
+	 */
+
+	public Person findPersonByID(int id) {
+
 		Connection con = DBConnection.connection();
 
 		try {
-			
-		
-		Statement stmt = con.createStatement();
-		
-		ResultSet rs = stmt.executeQuery(
-				"SELECT ID FROM person " + " WHERE ID= " + id );
-				 
-		
-		if (rs.next()) {
-			Person p = new Person();
-			p.setID(rs.getInt("ID"));
-			
-			return p;
+
+			Statement stmt = con.createStatement();
+
+			ResultSet rs = stmt.executeQuery("SELECT ID FROM person " + " WHERE ID= " + id);
+
+			if (rs.next()) {
+				Person p = new Person();
+				p.setID(rs.getInt("ID"));
+
+				return p;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
 		}
-		
-	}catch (SQLException e) {
-		e.printStackTrace();
 		return null;
 	}
-	return null;
-	}
-	
-/**
- *  Einf�gen eines neuen Objktes vom Typ Person in die DB
- *  der PK wird �berpr�ft und korrigiert -> maxID +1 
- * @param p 
- * die zu speichernde Person
- * @return
- * die bereits �bergebene Person
- */
-	
-	public int insertPerson (Person p){
-		
-Connection con = DBConnection.connection(); 
-		
-		try{
-			
+
+	/**
+	 * Einf�gen eines neuen Objktes vom Typ Person in die DB der PK wird
+	 * �berpr�ft und korrigiert -> maxID +1
+	 * 
+	 * @param p
+	 *            die zu speichernde Person
+	 * @return die bereits �bergebene Person
+	 */
+
+	public int insertPerson(Person p) {
+
+		Connection con = DBConnection.connection();
+
+		try {
+
 			Statement stmt = con.createStatement();
-			
+
 			ResultSet rs = stmt.executeQuery("SELECT MAX(ID) AS maxID " + " FROM person ");
-			
+
 			if (rs.next()) {
-				
-				p.setID(rs.getInt("maxID") +1);
-				
+
+				p.setID(rs.getInt("maxID") + 1);
+
 				stmt = con.createStatement();
-				
-				stmt.executeUpdate(
-						" INSERT INTO person (ID)"
-						+ " VALUES (" + p.getID()+ ")");
-					 	
+
+				stmt.executeUpdate(" INSERT INTO person (ID)" + " VALUES (" + p.getID() + ")");
+
 			}
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 
 		}
-		
+
 		return p.getID();
 	}
-	
+
 	/**
-	 *  ein Objekt vom Typ Person wird aus der DB gel�scht 
+	 * ein Objekt vom Typ Person wird aus der DB gel�scht
+	 * 
 	 * @param p
-	 * 	die zu l�schende Person
+	 *            die zu l�schende Person
 	 */
-		
-	public void deletePerson(Person p){
+
+	public void deletePerson(Person p) {
 		Connection con = DBConnection.connection();
-		
+
 		try {
-			
+
 			Statement stmt = con.createStatement();
-			
+
 			stmt.executeUpdate("DELETE FROM person " + "WHERE ID=" + p.getID());
-		}catch (SQLException e){
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-		
-	}
-	
-	
 
+}
